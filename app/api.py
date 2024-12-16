4a1fb5a874433962ea9c7db76c878f973184eaef
Daniel Pérez <34033241+wien996@users.noreply.github.com>
2024-12-16 21:42:37 +0100

    Update api.py

--- app/api.py
+++ app/api.py
@@ -5,6 +5,10 @@ from flask import Flask
 from app import util
 from app.calc import Calculator
 
+from flask import Flask, jsonify, abort
+
+app = Flask(__name__)
+
 CALCULATOR = Calculator()
 api_application = Flask(__name__)
 HEADERS = {"Content-Type": "text/plain", "Access-Control-Allow-Origin": "*"}
@@ -33,21 +37,15 @@ def substract(op_1, op_2):
         return (str(e), http.client.BAD_REQUEST, HEADERS)
 
 
-@api_application.route("/calc/multiply/<op_1>/<op_2>", methods=["GET"])
-def multiply(op_1, op_2):
-    try:
-        num_1, num_2 = util.convert_to_number(op_1), util.convert_to_number(op_2)
-        return ("{}".format(CALCULATOR.multiply(num_1, num_2)), http.client.OK, HEADERS)
-    except TypeError as e:
-        return (str(e), http.client.BAD_REQUEST, HEADERS)
-        
+@app.route('/calc/multiply/<int:x>/<int:y>', methods=['GET'])
+def multiply(x, y):
+    return jsonify(x * y)
 
-@api_application.route("/calc/divide/<op_1>/<op_2>", methods=["GET"])
-def divide(op_1, op_2):
-    try:
-        num_1, num_2 = util.convert_to_number(op_1), util.convert_to_number(op_2)
-        if num_2 == 0:
-            return ("Divisor cannot be zero", http.client.NOT_ACCEPTABLE, HEADERS)
-        return ("{}".format(CALCULATOR.divide(num_1, num_2)), http.client.OK, HEADERS)
-    except TypeError as e:
-        return (str(e), http.client.BAD_REQUEST, HEADERS)
+@app.route('/calc/divide/<int:x>/<int:y>', methods=['GET'])
+def divide(x, y):
+    if y == 0:
+        abort(406, description="Divisor cannot be zero")
+    return jsonify(x / y)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
